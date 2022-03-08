d56958220505d0f86d8c5931a83e109cb12f610d
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-08 14:00:16 -0500

    Update retrieve-everything-from-a-table.sql

--- basic/retrieve-everything-from-a-table.sql
+++ basic/retrieve-everything-from-a-table.sql
@@ -1,5 +1,5 @@
 '''
-[Question Link] (https://pgexercises.com/questions/basic/selectall.html)
+https://pgexercises.com/questions/basic/selectall.html
 '''
 
 SELECT facid, name, membercost,guestcost,initialoutlay, monthlymaintenance FROM cd.facilities;
