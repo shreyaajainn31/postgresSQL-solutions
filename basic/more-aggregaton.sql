0bf36c1cc65e18d6e2236c526df715728f61f0c1
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-08 18:05:40 -0500

    Create more-aggregaton.sql

---
+++ basic/more-aggregaton.sql
@@ -0,0 +1,5 @@
+'''
+https://pgexercises.com/questions/basic/agg2.html
+You'd like to get the first and last name of the last member(s) who signed up - not just the date. How can you do that?
+'''
+SELECT firstname, surname, joindate FROM cd.members WHERE joindate = (SELECT MAX(joindate) FROM cd.members);
