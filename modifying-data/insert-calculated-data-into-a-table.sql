be2c8ce5ad5ae8c5940e8ceb2d0b5c653853468b
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-09 00:08:20 -0500

    Create insert-calculated-data-into-a-table.sql

---
+++ modifying-data/insert-calculated-data-into-a-table.sql
@@ -0,0 +1,8 @@
+'''
+https://pgexercises.com/questions/updates/insert3.html
+Let's try adding the spa to the facilities table again. This time, though, we want to automatically generate the value for the next facid, rather than specifying it as a constant. Use the following values for everything else:
+Name: 'Spa', membercost: 20, guestcost: 30, initialoutlay: 100000, monthlymaintenance: 800.
+'''
+insert into cd.facilities
+    (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+    select (select max(facid) from cd.facilities)+1, 'Spa', 20, 30, 100000, 800;  
