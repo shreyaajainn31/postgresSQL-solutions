5e9a3f16a4f30ff7bf18989ce2843a9e6e759a1e
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-09 00:03:06 -0500

    Create delete-all-bookings.sql

---
+++ modifying-data/delete-all-bookings.sql
@@ -0,0 +1,6 @@
+'''
+https://pgexercises.com/questions/updates/delete.html
+As part of a clearout of our database, we want to delete all bookings from the cd.bookings table. How can we accomplish this?
+
+'''
+DELETE FROM cd.bookings;
