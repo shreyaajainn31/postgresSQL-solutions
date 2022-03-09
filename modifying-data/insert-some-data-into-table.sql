8d992f48162ed34f23e421ef86eac5dbe41edf40
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-08 23:39:00 -0500

    Create insert-some-data-into-table.sql

---
+++ modifying-data/insert-some-data-into-table.sql
@@ -0,0 +1,6 @@
+'''
+https://pgexercises.com/questions/updates/insert.html
+The club is adding a new facility - a spa. We need to add it into the facilities table. Use the following values:
+facid: 9, Name: 'Spa', membercost: 20, guestcost: 30, initialoutlay: 100000, monthlymaintenance: 800.
+'''
+insert into cd.facilities values(9,'Spa', 20,30,100000,800);
