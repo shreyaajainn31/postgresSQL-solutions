5eeb4f0b86af2f2bc3a7a096d781ef85d592102d
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-08 23:45:37 -0500

    Create update-some-existing-data.sql

---
+++ modifying-data/update-some-existing-data.sql
@@ -0,0 +1,6 @@
+'''
+https://pgexercises.com/questions/updates/update.html
+We made a mistake when entering the data for the second tennis court. The initial outlay was 10000 rather than 8000: you need to alter the data to fix the error.
+'''
+
+UPDATE cd.facilities SET initialoutlay = 10000 WHERE name = 'Tennis Court 2';
